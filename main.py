32df34464b0100a8a6e23bd2ceb7bd001c319342
Fran Moreno <fran_99_cbm@hotmail.com>
2021-10-20 13:55:48 +0200

    Testing with kidney tissue patches

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import cv2
 import numpy as np
 from tensorflow.keras.utils import normalize
 import matplotlib.pyplot as plt
+import os
+from sklearn.model_selection import train_test_split
+from unet_train import get_images
+import random
+from tqdm import tqdm
+from skimage import io
+from PIL import Image
 
 patch_size = 256
 
@@ -11,31 +18,108 @@ def get_model():
     return unet_model(patch_size, patch_size, 1)
 
 
-if __name__ == '__main__':
+def test(model):
+    im_path = "images/"
 
+    # 1. Get data
+    imFolders = [im_path+i for i in os.listdir(im_path) if os.path.isdir(im_path+i)]
+    imFolders.sort()
+    # testing = get_images(imFolders[0])
+    # testing_gt = get_images(imFolders[1])
+    image_dataset = get_images(imFolders[2])
+    mask_dataset = get_images(imFolders[3])
+
+    image_dataset = np.expand_dims(normalize(np.array(image_dataset), axis=1), 3)
+    mask_dataset = np.expand_dims((np.array(mask_dataset)), 3) / 255.
+
+    # 2. Train model
+    xtrain, xtest, ytrain, ytest = train_test_split(image_dataset, mask_dataset, test_size=0.10)
+
+    # 4. Compute IoU score
+    ypred = model.predict(xtest)
+    ypred_th = ypred > 0.5
+
+    intersection = np.logical_and(ytest, ypred_th)
+    union = np.logical_or(ytest, ypred_th)
+    iou_score = np.sum(intersection) / np.sum(union)
+    print("IoU score is ", iou_score)
+
+    # 4. Show prediction example
+    test_img_number = random.randint(0, len(xtest))
+    test_img = xtest[test_img_number]
+    ground_truth = ytest[test_img_number]
+    test_img_norm = test_img[:, :, 0][:, :, None]
+    test_img_input = np.expand_dims(test_img_norm, 0)
+    prediction = (model.predict(test_img_input)[0, :, :, 0] > 0.5).astype(np.uint8)
+
+    plt.figure(figsize=(16, 8))
+    plt.subplot(131)
+    plt.title('Testing image')
+    plt.imshow(test_img[:, :, 0], cmap="gray")
+    plt.subplot(132)
+    plt.title('Testing label')
+    plt.imshow(ground_truth[:, :, 0], cmap="gray")
+    plt.subplot(133)
+    plt.title('Prediction on test image')
+    plt.imshow(prediction, cmap="gray")
+    plt.savefig("prediction.png")
+
+
+def test2():
     model = get_model()
 
     # Evaluate the model
     weights_fname = 'mitochondria_test.hdf5'  # 'mitochondria_test.hdf5' | 'last.hdf5'
     model.load_weights(weights_fname)
 
-    fname = 'to_fill'
-    test_im = cv2.imread(fname, 0)
-    test_im_norm = np.expand_dims(normalize(np.array(test_im), axis=1), 3)
-    test_im_norm = test_im_norm[:, :, 0][:, :, None]
-    test_im_input = np.expand_dims(test_im_norm, 0)
+    im_path = 'myImages/'
+    imFolders = [im_path+i for i in os.listdir(im_path) if os.path.isdir(im_path+i)]
+    imFolders.sort()
+    ims = []
+    for imFolder in imFolders:
+        ims.extend(get_images(imFolder))
+    ims_dataset = np.expand_dims(normalize(np.array(ims), axis=1), 3)
+    prediction = (model.predict(ims_dataset)[:, :, :, 0] > 0.5).astype(np.uint8)
+    for i in range(len(prediction)):
+        plt.figure(figsize=(10, 4))
+        plt.subplot(121)
+        plt.title('test image')
+        plt.imshow(ims[i], cmap="gray")
+        plt.subplot(122)
+        plt.title('prediction')
+        plt.imshow(prediction[i], cmap="gray")
+        plt.show()
 
-    prediction = (model.predict(test_im_input)[0, :, :, 0] > 0.5).astype(np.uint8)
 
-    plt.figure(figsize=(16, 8))
-    plt.subplot(121)
-    plt.title('Testing image')
-    plt.imshow(test_im[:, :, 0], cmap="gray")
-    plt.subplot(122)
-    plt.title('Prediction on test image')
-    plt.imshow(prediction, cmap="gray")
-    plt.savefig("my_prediction.png")
+def get_subpatches_glomeruli(data_path: str):
+    dataNames = [i for i in os.listdir(data_path) if i.endswith('.png')]
+    patch_size_or = patch_size * 5
+    for dataName in tqdm(dataNames):
+        im = io.imread(data_path + dataName)
+        dirName = data_path + dataName[:-4] + '/'
+        if not os.path.isdir(dirName):
+            os.mkdir(dirName)
 
+        [h, w, _] = im.shape
+        patch_num = 0
+        stride = 0
+        for x in range(0, w, patch_size_or-stride):
+            if x+patch_size_or >= w:
+                x = w - patch_size_or
+            for y in range(0, h, patch_size_or-stride):
+                if y+patch_size_or >= h:
+                    y = h - patch_size_or
+                patch = Image.fromarray(im[y:y+patch_size_or, x:x+patch_size_or, :])
+                patch = patch.resize((patch_size, patch_size))
+                patch_name = str(patch_num).rjust(6, '0') + '.png'
+                if not os.path.isfile(dirName + patch_name):
+                    patch.save(dirName + patch_name)
+                patch_num += 1
+
+
+if __name__ == '__main__':
+    # get_subpatches_glomeruli('myImages/')  # Run just ONCE
+    test2()
 
 
 
