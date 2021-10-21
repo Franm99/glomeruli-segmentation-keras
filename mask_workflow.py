732756b4bbd1e566383fbc58f3ac2fa79dec1711
Fran Moreno <fran_99_cbm@hotmail.com>
2021-10-21 20:42:31 +0200

    Workflow for different image formats

--- mask_workflow.py
+++ mask_workflow.py
@@ -11,79 +11,91 @@ from PIL import Image
 from typing import List
 import time
 import pandas as pd
+from utils import show_ims
 
 PATCH_SIZE = 256
 
 
 def get_model():
+    """ returns the U-net model architecture for default input size."""
     return unet_model(PATCH_SIZE, PATCH_SIZE, 1)
 
 
-def get_ims_path(ims_dir : str) -> List[str]:
+def get_ims_path(ims_dir: str) -> List[str]:
+    """
+    Returns full paths to images (*.png format) in specified folder
+    :param ims_dir: folder to search in
+    :return: list of filenames
+    """
     return [ims_dir + i for i in os.listdir(ims_dir) if i.endswith('.png')]
 
 
-def get_mask(im : np.array, patch_org_size : int, format="gray") -> List[np.array]:
-    [h, w, _] = im.shape
+def get_mask(img: np.array, dim: int, im_format: str = "gray") -> List[np.array]:
+    """
+    Returns the predicted mask for a given renal tissue patch.
+    The input image can not be fed directly to the U-net model, as it has bigger dimensions than expected.
+    It is neccesary to divide the image into patches with (256, 256) size.
+    The number of masks returned by this functions depends on the number of channels (1 or 3) of the desired format.
+    :param img: original image
+    :param dim: desired size for (squared) patches in original format. Later, it will be resized to default model dims.
+    :param im_format: "gray", "RGB" or "HSV". Both RGB and HSV will return a list of 3 masks, one for each channel.
+    :return:list of masks by channel.
+    """
+    [h, w, _] = img.shape
+    # Initializing list of masks
     mask = [np.zeros((h, w), dtype=bool)]
-    if format != "gray":
-        mask = mask * 3
-    for x in range(0, w, patch_org_size):
-        if x + patch_org_size >= w:
-            x = w - patch_org_size
-        for y in range(0, h, patch_org_size):
-            if y + patch_org_size >= h:
-                y = h - patch_org_size
+    if im_format != "gray":
+        mask = [np.zeros((h, w), dtype=bool) for _ in range(3)]
 
-            # 2.2. Get sub-patch in original size
-            patch = im[y:y + patch_org_size, x:x + patch_org_size, :]
+    # Loop through the whole in both dimensions
+    for x in range(0, w, dim):
+        if x + dim >= w:
+            x = w - dim
+        for y in range(0, h, dim):
+            if y + dim >= h:
+                y = h - dim
 
-            # 2.3. U-Net works with (256,256) grayscale images:
-            # TODO: instead of converting RGB to grayscale, use different channels (RGB or HSV) and check results
-            patch_channels = get_channels(patch, format=format)  # Change to desired format
-            # patch = cv2.resize(cv2.cvtColor(patch, cv2.COLOR_RGB2GRAY),
-            #                    (PATCH_SIZE, PATCH_SIZE), interpolation=cv2.INTER_AREA)
+            # Get sub-patch in original size
+            patch = img[y:y + dim, x:x + dim, :]
+            patch_channels = get_channels(patch, im_format=im_format)  # Change to desired format
 
-            for ch, patch_ch in enumerate(patch_channels):
-            # 3. Use median filter on histogram to find non-tissue patches
-                counts, bins = np.histogram(patch_ch.flatten(), list(range(256 + 1)))
+            for idx, patch_channel in enumerate(patch_channels):
+
+                # Median filter applied on image histogram to discard non-tissue sub-patches
+                counts, bins = np.histogram(patch_channel.flatten(), list(range(256+1)))
                 counts.sort()
                 median = np.median(counts)
-                if median <= 3.0:
-                    # If sub-patch does not contain tissue, prediction mask is filled with 0's
-                    prediction_rs = np.zeros((patch_org_size, patch_org_size), dtype=np.uint8)
+                if median <= 3.:
+                    # Non-tissue sub-patches automatically get a null mask
+                    prediction_rs = np.zeros((dim, dim), dtype=np.uint8)
                 else:
-                    # 4. Use U-net to predict masks for sub-patches containing tissue.
-                    # 4.1. Make prediction (firstly, normalize dimensions)
-                    patch_input = np.expand_dims(normalize(np.array([patch_ch]), axis=1), 3)
-                    # TODO: Fix problem here: always getting same channel
+                    # Tissue sub-patches are fed to the U-net model for mask prediction
+                    patch_input = np.expand_dims(normalize(np.array([patch_channel]), axis=1), 3)
                     prediction = (model.predict(patch_input)[:, :, :, 0] > 0.5).astype(np.uint8)
-                    prediction_rs = cv2.resize(prediction[0], (patch_org_size, patch_org_size),
-                                                    interpolation=cv2.INTER_AREA)
+                    prediction_rs = cv2.resize(prediction[0], (dim, dim),
+                                               interpolation=cv2.INTER_AREA)
 
-                # 4.2. Upload patch mask using bool to easier merge redundant patches
-                mask[ch][y:y + patch_org_size, x:x + patch_org_size] = np.logical_or(
-                    mask[ch][y:y + patch_org_size, x:x + patch_org_size],
-                    prediction_rs.astype(np.bool))
-
-        for i in range(len(mask)):
-            plt.figure()
-            plt.imshow(mask[i], cmap="gray")
-        plt.show()
-        plt.close()
-
-    return [mask[ch].astype(np.uint8) for ch in range(len(mask))]
+                # Final mask is composed by the sub-patches masks (boolean format)
+                mask[idx][y:y+dim, x:x+dim] = \
+                    np.logical_or(mask[idx][y:y+dim, x:x+dim], prediction_rs.astype(np.bool))
+    return [mask[ch].astype(np.uint8) for ch in range(len(mask))] # Change datatype from np.bool to np.uint8
 
 
-def get_channels(patch : np.array, format : str ="gray"):
-    if format == "gray":
+def get_channels(patch: np.array, im_format: str ="gray"):
+    """
+    Get channels from input image (patch) in the specified format
+    :param patch: patch to get channels from
+    :param im_format: "gray", "RGB", "HSV"
+    :return: list of channels resized to default dimensions for the U-Net model
+    """
+    if im_format == "gray":
         return [cv2.resize(cv2.cvtColor(patch, cv2.COLOR_RGB2GRAY),
                            (PATCH_SIZE, PATCH_SIZE), interpolation=cv2.INTER_AREA)]
-    elif format == "hsv" or format == "HSV":
+    elif im_format == "hsv" or im_format == "HSV":
         patch = cv2.resize(cv2.cvtColor(patch, cv2.COLOR_RGB2HSV),
                            (PATCH_SIZE, PATCH_SIZE), interpolation=cv2.INTER_AREA)
         return [patch[:, :, i] for i in range(patch.shape[2])]
-    elif format == "rgb" or format == "RGB":
+    elif im_format == "rgb" or im_format == "RGB":
         patch = cv2.resize(patch, (PATCH_SIZE, PATCH_SIZE), interpolation=cv2.INTER_AREA)
         return [patch[:, :, i] for i in range(patch.shape[2])]
 
@@ -108,17 +120,27 @@ if __name__ == '__main__':
     for im_name in ims_names:
         # 2.1. Using OpenCV to read images in array format
         im = cv2.cvtColor(cv2.imread(im_name, cv2.IMREAD_COLOR), cv2.COLOR_BGR2RGB)
-        mask = get_mask(im, patch_org_size, format="hsv")
+        mask = get_mask(im, patch_org_size, im_format="rgb")
+        # TODO: save images to disk
         masks.append(mask)
 
         # 6. Show results (end loop)
-        plt.figure(figsize=(8, 8))
-        plt.subplot(121)
-        plt.title("Patch")
+        ims = [im] + mask
+        # show_ims(ims, 1, len(ims))
+        plt.figure()
+        plt.subplot(141)
         plt.imshow(im)
-        plt.subplot(122)
-        plt.title("Mask")
-        plt.imshow(mask, cmap="gray")
+        plt.title("Original")
+        plt.subplot(142)
+        plt.imshow(mask[0], cmap="gray")
+        plt.title("H")
+        plt.subplot(143)
+        plt.imshow(mask[1], cmap="gray")
+        plt.title("S")
+        plt.subplot(144)
+        plt.imshow(mask[2], cmap="gray")
+        plt.title("V")
         plt.show()
         plt.close()
+
 
