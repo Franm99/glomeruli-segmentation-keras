3456d45265088c7b8d48e1c3ac3d8ff5dfadf073
Fran Moreno <fran_99_cbm@hotmail.com>
2021-11-05 11:51:11 +0100

    Class to generate rounded masks masks_circles.py contains a test snippet to generate rounded masks with different size radii. Each set of masks is saved to disk with a size label.

--- mask_generator/masks_circles.py
+++ mask_generator/masks_circles.py
@@ -2,27 +2,55 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2.cv2 as cv2
 from typing import Tuple, List
+import os, glob
+from bs4 import BeautifulSoup
+from tqdm import tqdm
 
-GLOMERULI_SIZE = 600  # Empirically defined glomeruli size limit
+GLOMERULI_SIZE = 400  # Empirically defined glomeruli size limit
+DATA_PATH = "D:/DataGlomeruli"
 
 
 class MaskCreator:
     def __init__(self):
-        pass
+        self._ims = DATA_PATH + '/ims'
+        self._glomeruli_coords = DATA_PATH + '/glomeruli_coords'
+        self._masks = DATA_PATH + '/masks'
 
-    def get_mask(self, im: np.array):
-        # TODO: read centre points from XML
-        cs = [(50, 50), (1200, 1450), (300, 3000)]
-        mask = self.__create_rounded_masks(im, cs)
-        plt.figure()
-        plt.subplot(121)
-        plt.imshow(im)
-        plt.subplot(122)
-        plt.imshow(mask, cmap="gray")
-        plt.show()
+        self._xml_file_list = glob.glob(self._glomeruli_coords + '/*')
+        self._ims_file_list = glob.glob(self._ims + '/*')
+
+    def run(self, r: int = GLOMERULI_SIZE):
+        for xml_file, im_file in zip(self._xml_file_list, self._ims_file_list):
+            points = self._get_points_from_xml(xml_file)
+            im = self._get_im(im_file)
+            mask = self._get_rounded_mask(im, points, r)
+            # self._plot_sample(im, mask)
+            self._save_im(mask, im_file, r)
+
+    def _get_points_from_xml(self, xml_file: str) -> List[Tuple[int, int]]:
+        with open(xml_file, 'r') as f:
+            data = f.read()
+        bs_data = BeautifulSoup(data, "xml")
+        point_fields = bs_data.find_all('point')
+        p = []
+        for link in point_fields:
+            p.append((int(link.get('X')), int(link.get('Y'))))
+        return p
+
+    def _save_im(self, mask, im_file, r: int) -> None:
+        mask_name = os.path.basename(im_file).split('.')[0] + '_mask.png'
+        mask_dir = self._masks + '_' + str(r)
+        if not os.path.exists(mask_dir):
+            os.mkdir(mask_dir)
+        cv2.imwrite(os.path.join(mask_dir, mask_name), mask)
+
 
     @staticmethod
-    def __create_rounded_masks(im: np.array, cs: List[Tuple[int, int]], r: int = GLOMERULI_SIZE):
+    def _get_im(imfile: str) -> np.ndarray:
+        return cv2.cvtColor(cv2.imread(imfile, cv2.IMREAD_COLOR), cv2.COLOR_BGR2RGB)
+
+    @staticmethod
+    def _get_rounded_mask(im: np.array, cs: List[Tuple[int, int]], r: int) -> np.ndarray:
         """
         Create a binary mask where circles (255) mark the position of glomeruli in image.
         Source:  https://stackoverflow.com/questions/8647024/how-to-apply-a-disc-shaped-mask-to-a-numpy-array/8650741
@@ -34,16 +62,24 @@ class MaskCreator:
         h, w = im.shape[1], im.shape[0]
         im_mask = np.zeros((h, w), dtype=np.uint8)
         for c in cs:
-            y, x = np.ogrid[-c[0]:h-c[0], -c[1]:w-c[1]]
+            y, x = np.ogrid[-c[1]:h-c[1], -c[0]:w-c[0]]
             cmask = x*x + y*y <= r*r
             im_mask[cmask] = 255
         return im_mask
 
+    @staticmethod
+    def _plot_sample(im, mask):
+        plt.figure()
+        plt.imshow(im)
+        plt.imshow(mask, cmap="jet", alpha=0.2)
+        plt.show()
+        # plt.close()
+
 
 if __name__ == '__main__':
     maskCreator = MaskCreator()
-
-    impath = '21A3 A5 HE_x0y2400s3200.png'
-    img = cv2.imread(impath, cv2.IMREAD_COLOR)
-    maskCreator.get_mask(img)
+    radii = np.arange(250, 600, 50)
+    for radius in tqdm(radii, desc="Masks folders"):
+        maskCreator.run(radius)
+    # test()
 
