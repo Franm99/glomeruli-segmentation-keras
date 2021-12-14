0d5e0d56451adf459d05fa55bb5c4539e4e7e296
Fran Moreno <fran_99_cbm@hotmail.com>
2021-12-14 13:38:26 +0100

    Debug for GPU usage

--- testbench.py
+++ testbench.py
@@ -67,6 +67,22 @@ class TestBench:
             print_info("Batch size: {}".format(params.BATCH_SIZE))
             print_info("Patience for Early Stopping: {}".format(params.ES_PATIENCE))
             print_info("LAUNCHING TRAINING PROCESS:")
+
+            # DEBUG
+            import tensorflow as tf
+            gpus = tf.config.experimental.list_physical_devices('GPU')
+            if gpus:
+                try:
+                    # Currently, memory growth needs to be the same across GPUs
+                    for gpu in gpus:
+                        tf.config.experimental.set_memory_growth(gpu, True)
+                    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+                    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+                except RuntimeError as e:
+                    # Memory growth must be set before GPUs have been initialized
+                    print(e)
+
+
             history = model.fit(xtrain, ytrain, batch_size=params.BATCH_SIZE, verbose=1, epochs=params.EPOCHS,
                                 validation_data=(xval, yval), shuffle=False, callbacks=callbacks)
             print_info("TRAINING PROCESS FINISHED.")
